d10713012ad1e84365bf660eb8e72ceba3240f11
Stephen Gary <sgary0@protonmail.com>
2020-08-31 13:34:46 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+// import express from 'express' // ES2015 modules
+const express = require('express'); // CommonJS modules, similar to above
+
+const server = express();
+
+const hubs= [
+    {
+        id: 1,
+        name: 'node intro',
+        lessonId: 1,
+        cohort: 'node 33',
+    },
+    {
+        id: 2,
+        name: 'node server side routing',
+        lessonId: 2,
+        cohort: 'node 33',
+    },
+];
+
+server.get('/', (req, res) => {
+    res.status(200).json({ hello: 'Node 33' });
+});
+
+server.get('/hubs', (req, res) => {
+    res.status(200).json({ data: hubs });
+});
+
+server.post('/hubs', (req, res) => {
+
+})
+
+const port = 8000;
+server.listen(port, () => console.log(`***Server is running on port ${port}***`));
